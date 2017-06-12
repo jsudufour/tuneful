75ef97ce8ccd2197accf8516bdac0495b86177a4
jsudufour <jsudufour@gmail.com>
2017-06-11 23:26:36 -0400

    Create models for Song and File

--- tuneful/database.py
+++ tuneful/database.py
@@ -11,5 +11,31 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Create your models here
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True)
+    file_id = Column(Integer, ForeignKey('files.id'), nullable=False)
+
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "file": self.file.as_dictionary(),
+        }
+        return song
+
+class File(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    song = relationship("Song", backref="file")
+
+    def as_dictionary(self):
+        file = {
+            "id": self.id,
+            "name": self.name,
+        }
+        return file
 
 Base.metadata.create_all(engine)
